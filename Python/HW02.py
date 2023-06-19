de088372f0e88e63ea0eae5979904be260823621
Vincent Gerla <info@crrazyvinc.nl>
2023-06-19 12:26:43 +0200

    feat(Python): add list of street names and print specific street name and all streets with zip code 2514

---
+++ Python/HW02.py
@@ -0,0 +1,10 @@
+Streetnames = ["Lange Voorhout", "Noordeinde",
+               "Frederik Hendriklaan", "Denneweg",
+               "Plein", "Javastraat", "Prinsestraat",
+               "Scheveningseweg", "Spui",
+               "Laan van Meerdervoort"]
+
+print(Streetnames[2])
+
+for Street in Streetnames:
+    print("2514", Street)
