a2e1a5a62fb586a6dbc43d3b1f2f8991eff6c323
Vincent Gerla <info@crrazyvinc.nl>
2023-06-19 12:07:47 +0200

    feat(main.py): add Python script to prompt user for name, birthplace and age and print them. If age is greater than 50, print a joke.

---
+++ Python/main.py
@@ -0,0 +1,8 @@
+Name = input("Naam: ")
+BirthPlace = input("Geboorteplaats: ")
+Age = input("Leeftijd: ")
+
+print(Name, BirthPlace, Age)
+
+if(int(Age) > 50):
+    print("Shaggy en Britney Spears zitten samen aan de bar en opeens laat één van de twee een scheet. “It wasn’t me”, roept Shaggy en Britney zegt: Oops, I did it again…")
