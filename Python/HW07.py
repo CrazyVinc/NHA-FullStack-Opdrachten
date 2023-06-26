584b7b3db772c45873121e5575ce8e1696af6174
Vincent Gerla <info@crrazyvinc.nl>
2023-06-26 11:07:32 +0200

    Added HW07

---
+++ Python/HW07.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+fruitsoorten = "appel, banaan, sinaasappel, druif, kiwi, aardbei, ananas, mango, peer, framboos"
+fruitsoorten = fruitsoorten.split()
+
+for i in range(5):
+    print(fruitsoorten[i])
+    # print(i+1, fruitsoorten[i])
+    
+print('\n')
+for i in range(50):
+    print(i)
