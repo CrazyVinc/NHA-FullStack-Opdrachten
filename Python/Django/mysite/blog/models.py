ffdecabd761fc140a5a527b8949523549b5a0167
Vincent Gerla <info@crrazyvinc.nl>
2023-12-29 22:04:17 +0100

    HW opdracht

--- Python/Django/mysite/blog/models.py
+++ Python/Django/mysite/blog/models.py
@@ -1,13 +1,17 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Voetbalspelers(models.Model):
-    naam_voetballer = models.CharField(max_length=255)
-    actuele_voetbalclub = models.CharField(max_length=255)
-    naam_auteur = models.CharField(max_length=255)
-    datum_invoer = models.DateTimeField(default=timezone.now)
-    datum_aanpassing = models.DateTimeField(default=timezone.now)
-
-    def save_entry(self):
-        self.datum_aanpassing = timezone.now()
-        self.save()
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+    
+def __str__(self):
+    return self.title
