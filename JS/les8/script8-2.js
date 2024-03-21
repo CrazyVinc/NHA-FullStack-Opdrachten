ea35d80371cca5de898928e2d013a209ec9c8e0e
Vincent Gerla <vlgerlanl@gmail.com>
2024-03-21 18:27:33 +0100

    Les 8

---
+++ JS/les8/script8-2.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("div").css("background-color", "lightgray");
+    $("div").css("color", "black");
+});
---
+++ JS/les8/script8-3.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("tr:odd").css("background-color", "lightblue");
+    $("tr:even").css("background-color", "lightgreen");
+});
